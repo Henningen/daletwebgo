abfeb2cffc975c5fbb5778ca398fdb3353a74b98
henning <henning.ervik@gmail.com>
2016-03-27 16:35:21 +0200

    added some more files, layout and template, still does not do anything

--- daletwebgo/wavfinder/views.py
+++ daletwebgo/wavfinder/views.py
@@ -5,5 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the wavfinder index.")
+def wavsearch(request):
+#    return HttpResponse("Hello, world. You're at the wavfinder index.")
+    return render(request, 'wavfinder/wavsearch.html', {})
