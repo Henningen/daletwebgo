bc1a12be8c49bf9c28daa231a9f0d957348635be
henning <henning.ervik@gmail.com>
2016-03-28 02:16:21 +0200

    changed from get to post form and made urls more logic with a redirect

--- daletwebgo/wavfinder/urls.py
+++ daletwebgo/wavfinder/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.wavsearch, name='wavsearch'),
+    url(r'^search/$', views.wavsearchredir, name='wavsearchredir'),
+    url(r'^search/(?P<search>[^/]+)/$', views.wavsearch, name='wavsearch'),
     url(r'^jobstatus/$', views.jobstatus, name='jobstatus'),
     url(r'^jobstatus/(?P<status>(progress|failed|finished|all))/$', views.jobstatus, name='jobstatus'),
 ]
--- daletwebgo/wavfinder/views.py
+++ daletwebgo/wavfinder/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from stat import S_ISREG, ST_MTIME, ST_MODE
 import os, sys, time, re
 
@@ -11,7 +11,7 @@ class Search(object):
     query=""
     results=0
 
-def wavsearch(request ):
+def wavsearch(request, search="" ):
 #    return HttpResponse("Hello, world. You're at the wavfinder index.")
     dirpath="/home/henning/env/inputfiles"  # insert the path to your directory, FIXME: Please make static variable of some sort for this
     entries = (os.path.join(dirpath, fn) for fn in os.listdir(dirpath))
@@ -19,23 +19,28 @@ def wavsearch(request ):
     entries = ((os.stat(path), path) for path in entries)
     # leave only regular files, insert creation date
     entries = ((stat[ST_MTIME], path) for stat, path in entries if S_ISREG(stat[ST_MODE]))
-    search = Search()
-    search.query=""
-    if request.GET.get('search'):
-       search.query = request.GET.get('search')
+    searchobject=Search()
+    searchobject.query=search
+    #if request.POST.get('search', ""):
+    #   searchobject.query = request.POST.get('search', "")
     filelist=[]
     count=0
     for time, path in sorted(entries, reverse=True):
-        if search.query:
-            if re.search(search.query, os.path.basename(path), re.IGNORECASE):
+        if searchobject.query:
+            if re.search(searchobject.query, os.path.basename(path), re.IGNORECASE):
                 filelist.append(os.path.basename(path))
-                search.results += 1
+                searchobject.results += 1
         else:
             filelist.append(os.path.basename(path))
         count+=1
         if count == 10:
             break
-    return render(request, 'wavfinder/wavsearch.html', {'filelist': filelist, 'search': search})
+    return render(request, 'wavfinder/wavsearch.html', {'filelist': filelist, 'search': searchobject})
+
+def wavsearchredir(request, search=""):
+    if request.POST.get('search', ""):
+       search = request.POST.get('search', "")
+    return HttpResponseRedirect( "/wavfinder/search/"+search+"/")
 
 
 def jobstatus(request, status="progress"):
